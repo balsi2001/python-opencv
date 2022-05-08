d954957fdfd56cdc2068bd1f88849b93edc9dcdf
John Doe <johndoe@example.com>
2022-05-08 21:31:16 +0800

    03

--- 0506/FourFunctionCmp.py
+++ 0506/FourFunctionCmp.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 src = cv2.imread('Baboon256.bmp',0)
+
 '''
 請讀取Baboon256圖檔, 分別使用Sobel, Canny（低閾值50, 高閾值=80), 以Laplacian(Ksize=3)及Scharr四個函數的來建立影像的邊緣。
 '''
@@ -18,13 +19,13 @@ dst_scharr =  cv2.addWeighted(dstx, 0.5,dsty, 0.5, 0)   # 影像融合
 # 輸出影像梯度
 dstmp=cv2.Laplacian(src,cv2.CV_32F,ksize=3)
 dst_lap=cv2.convertScaleAbs(dstmp)
+
 cv2.imshow("Src", src)       
 cv2.imshow("Sobel", dst_sobel)
 cv2.imshow('Scharr', dst_scharr) 
-cv2.imshow("Laplacian", dst_lap)    
-    
-ocan=np.zeros(src.shape,np.uint8)
-ocan=cv2.Canny(src, 50, 80, apertureSize = 3)
+cv2.imshow("Laplacian", dst_lap)      
+src=cv2.GaussianBlur(src,(3,3),2)
+ocan=cv2.Canny(src, 50, 80)
 cv2.imshow('canny',ocan)
 cv2.waitKey(0)      
 cv2.destroyAllWindows() 
